787e04d52ee58d8e11b6e588fee72207ed39cbd2
ofurum <chiagoziemofurum20@gmail.com>
2020-07-30 11:28:57 +1300

    my name was stated

---
+++ src/components/myName.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <section class="name">
+          <h1>{{ fullName }}</h1>
+      </section>
+  </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            fullName: 'Ofurum Chiagoziem'
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.name{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: rgb(96, 96, 148)
+}
+
+</style>
